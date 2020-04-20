74156ed913ea4c69ea90f4235a5ed37c62377575
skybrim <throughskybrim@gmail.com>
2020-04-20 16:41:50 +0800

    point

--- demo/demo/main.m
+++ demo/demo/main.m
@@ -9,9 +9,30 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+typedef void (^ByBlock)(void);
+@interface TestObj : NSObject
+@property (nonatomic, copy) ByBlock block;
+@end
+@implementation TestObj
+- (void)testMethod {
+//    __strong TestObj *sSelf = self;
+    if (self.block) {
+        self.block();
+    }
+    NSLog(@"%@", self);
+}
+@end
+
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
+        __block TestObj *testObj = [TestObj new];
+        testObj.block = ^{
+            testObj = nil;
+            NSLog(@"2");
+        };
+        [testObj testMethod];
+        
         // Setup code that might create autoreleased objects goes here.
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
     }
